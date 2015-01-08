c5c3cf401f589a50addcf10bfb776e53d1d8b36a
Scott Kinney <skinney6@gmail.com>
2015-01-08 21:50:16 +0000

    use private ip except for twit3's

--- ssh-servers-from-aws.rb
+++ ssh-servers-from-aws.rb
@@ -18,7 +18,7 @@ secret_access_key = config.match(/AWS_SECRET_KEY=(.+)/)[1]
 # Add more regions here if necessary. No harm in adding all of them, just makes the generation take longer to query more regions.
 regions = [
   'us-east-1',
-  'us-west-2'
+  'us-west-1'
 ]
 
 ssh_config = ''
@@ -31,9 +31,17 @@ regions.each do |region|
     if(instance.status == :running && instance.tags['Name'])
       instance_name = instance.tags['Name'].gsub /[^a-zA-Z0-9\-_\.]/, '-'
       instance_user = instance.tags['User'] || 'ubuntu'
-      puts "#{instance.id}: #{instance_name} #{instance.ip_address} (#{instance_user})"
+      if instance_name =~ /twit3/
+        puts "#{instance.id}: #{instance_name} #{instance.public_ip_address} (#{instance_user})"
+      else
+        puts "#{instance.id}: #{instance_name} #{instance.private_ip_address} (#{instance_user})"
+      end
       ssh_config << "Host #{instance_name}\n"
-      ssh_config << "  HostName #{instance.ip_address}\n"
+      if instance_name =~ /twit3/
+        ssh_config << "  HostName #{instance.public_ip_address}\n"
+      else
+        ssh_config << "  HostName #{instance.private_ip_address}\n"
+      end
       ssh_config << "  User #{instance_user}\n"
       if instance.key_name.end_with?('.pem')
         ssh_config << "  IdentityFile ~/.ssh/#{instance.key_name}\n"
