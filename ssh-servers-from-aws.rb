fcd843a7e7f29d4e1eee6f118f1bf3d8b01613a2
Scott Kinney <skinney6@gmail.com>
2015-01-10 16:17:20 -0800

    added opt to generate local ssh prozy config

--- ssh-servers-from-aws.rb
+++ ssh-servers-from-aws.rb
@@ -1,17 +1,37 @@
 #!/usr/bin/env ruby
 require 'aws-sdk'
+require 'optparse'
 
 Dir.chdir File.expand_path File.dirname(__FILE__)
 
-slug = ARGV.join
-
-config = File.read("aws/#{slug}.profile")
-
-if !config
-  puts "No config file found for #{slug}"
-  exit
+options = {}
+optparse = OptionParser.new do |opts|
+  options[:profile] = "" 
+  opts.on( '-f', '--profile FILE', "Profile name" ) do |f| 
+    options[:profile] = f 
+  end 
+  options[:proxy] = false
+  opts.on('-p', '--proxy', 'SSH proxy thru jump host') do
+    options[:proxy] = true
+  end
+  options[:jump_user] = 'ubuntu'
+  opts.on('-u', '--user USER_NAME', 'The username you log into jump with') do |username|
+    options[:jump_user] = username
+  end
+  options[:jump_key] = 'id_rsa'
+  opts.on('-k', '--key KEY_NAME', 'The key you log into jump with') do |key|
+    options[:jump_key] = key
+  end
+  opts.on( '-h', '--help', 'Display this screen' ) do 
+    puts opts 
+    exit 
+  end 
 end
 
+optparse.parse! 
+puts options
+config = File.read("aws/#{options[:profile]}.profile")
+    
 access_key_id = config.match(/AWS_ACCESS_KEY=(.+)/)[1]
 secret_access_key = config.match(/AWS_SECRET_KEY=(.+)/)[1]
 
@@ -21,39 +41,49 @@ regions = [
   'us-west-1'
 ]
 
-ssh_config = ''
+if options[:proxy]
+  ssh_config = "Host jump.zenti.com\n  Port 19750\n  HostName jump.zenti.com\n  User #{options[:jump_user]}\n  IdentityFile ~/.ssh/#{options[:jump_key]}\n\n"
+else
+  ssh_config = ''
+end
 
 regions.each do |region|
   AWS.config access_key_id: access_key_id, secret_access_key: secret_access_key, region: region
   ec2 = AWS.ec2
-  
+
   ec2.instances.each do |instance|
+
     if(instance.status == :running && instance.tags['Name'])
       instance_name = instance.tags['Name'].gsub /[^a-zA-Z0-9\-_\.]/, '-'
       instance_user = instance.tags['User'] || 'ubuntu'
-      if instance_name =~ /twit3/
-        puts "#{instance.id}: #{instance_name} #{instance.public_ip_address} (#{instance_user})"
+      if instance_name == 'jump.zenti.com'
+        next
       else
-        puts "#{instance.id}: #{instance_name} #{instance.private_ip_address} (#{instance_user})"
+        if instance_name =~ /twit3/
+          puts "#{instance.id}: #{instance_name} #{instance.public_ip_address} (#{instance_user})"
+        else
+          puts "#{instance.id}: #{instance_name} #{instance.private_ip_address} (#{instance_user})"
+        end
+        ssh_config << "Host #{instance_name}\n"
+        if instance_name =~ /twit3/
+          ssh_config << "  HostName #{instance.public_ip_address}\n"
+        else
+          ssh_config << "  HostName #{instance.private_ip_address}\n"
+          ssh_config << "  ProxyCommand ssh jump.zenti.com -W %h:%p\n" if options[:proxy]
+        end
+        ssh_config << "  User #{instance_user}\n"
+        if instance.key_name.end_with?('.pem')
+          ssh_config << "  IdentityFile ~/.ssh/#{instance.key_name}\n"
+        else
+          ssh_config << "  IdentityFile ~/.ssh/#{instance.key_name}.pem\n"
+        end
+        ssh_config << "\n"
       end
-      ssh_config << "Host #{instance_name}\n"
-      if instance_name =~ /twit3/
-        ssh_config << "  HostName #{instance.public_ip_address}\n"
-      else
-        ssh_config << "  HostName #{instance.private_ip_address}\n"
-      end
-      ssh_config << "  User #{instance_user}\n"
-      if instance.key_name.end_with?('.pem')
-        ssh_config << "  IdentityFile ~/.ssh/#{instance.key_name}\n"
-      else
-        ssh_config << "  IdentityFile ~/.ssh/#{instance.key_name}.pem\n"
-      end
-      ssh_config << "\n"
     end
   end
 end
 
-ssh_file = "ssh/#{slug}.sshconfig"
+ssh_file = "ssh/#{options[:profile]}.sshconfig"
 File.open(ssh_file, 'w') {|f| f.write(ssh_config) }
 
 puts "Complete! Now run rebuild-ssh-config.sh to update your .ssh/config file"
